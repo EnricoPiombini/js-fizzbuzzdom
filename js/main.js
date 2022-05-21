3dcbf7201f31fe7a20450c64db9889a92e5956e6
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-21 15:13:51 +0200

    innerHtml

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-// Dichiaro la variabile con cui identifico il div in cui andrò i inserire i valori 
+// Dichiaro la variabile con cui identifico il div in cui andrò a inserire i valori 
 const boxHtml = document.querySelector(".box");
+
+
 console.log(boxHtml);
 
-// Genero il loop
-for(let i = 0; i <= 100; i++){
-    if (i % 15 ===0) console.log("FizzBuzz");
-    else if (i % 3 === 0) console.log("Fizz");
-    else if (i % 5 === 0) console.log("Buzz");
-    console.log(i);
+let testo = "";
 
 
-}
+
+// //Genero il loop
+for (let i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        testo = 'FizzBuzz';
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 === 0) {
+        testo = 'Fizz';
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        testo = 'Buzz';
+        console.log("Buzz");
+    }
+    else {
+        testo = i
+        console.log(i);
+    }
+
+    boxHtml.innerHTML += `<div> ${testo}</div>  `
+
+};
+
