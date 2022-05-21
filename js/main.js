8136b254aaeca9de04a3e27b7ab112640e77ec13
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-21 16:27:40 +0200

    Box Css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 const boxHtml = document.querySelector(".box");
 
 
+
+
+
+
 console.log(boxHtml);
 
 let testo = "";
@@ -11,23 +15,31 @@ let testo = "";
 // //Genero il loop
 for (let i = 1; i <= 100; i++) {
     if (i % 15 === 0) {
+
+
         testo = 'FizzBuzz';
         console.log("FizzBuzz")
     }
     else if (i % 3 === 0) {
+
+
         testo = 'Fizz';
         console.log("Fizz");
     }
     else if (i % 5 === 0) {
+
+
         testo = 'Buzz';
         console.log("Buzz");
     }
     else {
+
+
         testo = i
         console.log(i);
     }
 
-    boxHtml.innerHTML += `<div> ${testo}</div>  `
+    boxHtml.innerHTML += `<div class= "smallBox">${testo}</div>  `
 
 };
 
