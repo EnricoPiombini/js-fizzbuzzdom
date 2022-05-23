18ccf2b9fbefe679b9365ae187e36695c675eec7
EnricoPiombini <piombini.enrico@gmail.com>
2022-05-23 10:24:31 +0200

    Colors added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,30 @@
 // Dichiaro la variabile con cui identifico il div in cui andrò a inserire i valori 
 const boxHtml = document.querySelector(".box");
-
-
-
-
-
-
 console.log(boxHtml);
-
 let testo = "";
+let backColor = "";
 
 
 
-// //Genero il loop
+//Genero il loop
 for (let i = 1; i <= 100; i++) {
+backColor = ("number");
+
     if (i % 15 === 0) {
-
-
         testo = 'FizzBuzz';
+        backColor = ("fizzbuzz");
+
+
         console.log("FizzBuzz")
     }
     else if (i % 3 === 0) {
-
-
         testo = 'Fizz';
+        backColor = ("fizz");
         console.log("Fizz");
     }
     else if (i % 5 === 0) {
-
-
         testo = 'Buzz';
+        backColor = ("buzz");
         console.log("Buzz");
     }
     else {
@@ -39,7 +34,12 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 
-    boxHtml.innerHTML += `<div class= "smallBox">${testo}</div>  `
+    boxHtml.innerHTML += `<div class="${backColor}">
+    ${testo}</div> `
 
 };
+
+
+
+
 
